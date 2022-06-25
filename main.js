e3e1cfb6310fe371216cd380ae106d2b76cb526b
alexmmosac <alexmmosac@outlook.com>
2022-06-25 16:21:50 -0400

    Added: sketch ability, clear grid, resize grid. TODO: add rainbow mode.

--- main.js
+++ main.js
@@ -1,21 +1,48 @@
 const container = document.getElementById("container");
+var w = window.innerWidth;
+var h = window.innerHeight;
+
 
 function makeRows(rows, cols) {
+cleargrid();
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
+    container.appendChild(cell).id = c + 1;
     container.appendChild(cell).className = "grid-item";
   }
+  setEventListener();
+}
+//Destroy the grid
+function cleargrid(){
+    container.innerHTML = '';
 }
 
-makeRows(16, 16);  //default grid
-//makeRows(64,64);
-//TODO: Implent Function/Event Listener to work change color on mouse over. 
-/*
-var cell = document.getElementsByClassName("grid-item");
-cell.addEventListener('mouseover', function handleMouseOver() {
-    console.log("TEST CHANGED");
-    cell.style.color = "black";
-});
-*/
+function colorPicker(){
+    let colors = ["black", "red", "blue", "green", "yellow", "purple", "orange"];
+    var black = colors[1];
+}
+
+
+
+// Below is code for sketching on page/ mobile. 
+function sketch() {
+    this.style.backgroundColor = "black";
+}
+
+function setEventListener(){
+var canvas_gridmap = container.querySelectorAll('div');
+if(w >= 1500){
+canvas_gridmap.forEach(canvas_gridmap => canvas_gridmap.addEventListener('mouseover', sketch));
+}
+else{
+canvas_gridmap.forEach(canvas_gridmap => canvas_gridmap.addEventListener('touchstart', sketch));
+}
+}
+
+// clears sketch
+function clearCanvas(){
+    var canvas_gridmap = container.querySelectorAll('div');
+    canvas_gridmap.forEach(canvas_gridmap => canvas_gridmap.style.backgroundColor = '#b3b3b3');
+}
